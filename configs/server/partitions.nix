9214a99d5a323043e29b37ec2236f1042703aae2
trivaris <github@tripple.lurdane.de>
2025-08-18 10:19:54 +0200

    feat: Changed Server Disk Layout

--- configs/server/partitions.nix
+++ configs/server/partitions.nix
@@ -1,47 +1,55 @@
 { lib, ... }:
 {
-  disko.devices.disk.main = {
-    type = "disk";
-    device = lib.mkDefault "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_102062030";
-
-    content = {
-      type = "table";
-      format = "msdos";
-      partitions = [
-        {
-          name = "boot";
-          part-type = "primary";
-          start = "1MiB";
-          end = "512MiB";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/boot";
+  disko.devices = {
+    disk.nixos = {
+      device = lib.mkDefault "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_102062030";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
           };
-        }
-        {
-          name = "swap";
-          part-type = "primary";
-          start = "512MiB";
-          end = "4G";
-          content = {
-            type = "swap";
-            priority = 100;
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
           };
-        }
-        {
-          name = "root";
-          part-type = "primary";
-          start = "4.5GiB";
-          end = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
           };
-        }
-      ];
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+        };
+      };
     };
   };
 }
