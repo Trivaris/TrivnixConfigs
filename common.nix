551daa79c9f55a2ba8ba8179489be96c16af8db0
trivaris <git@trivaris.org>
2025-09-23 22:15:10 +0200

    update: update

--- common.nix
+++ common.nix
@@ -8,6 +8,10 @@
     git = {
       email = "git@trivaris.org";
       enableSigning = true;
+      urlAliases = {
+        "git@github.com:Trivaris/" = "triv:";
+        "git@github.com:" = "github:";
+      };
     };
 
     librewolf = {
