560c86c1e2e680ec4ef96abed1f14427788fb38d
trivaris <git@trivaris.org>
2026-01-07 16:14:57 +0100

    fix: Cleanup

--- configs/lab/users.nix
+++ configs/lab/users.nix
@@ -1,7 +1,11 @@
 { common, ... }:
 {
   trivaris = {
-    infos.hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
+    infos = {
+      name = "trivaris";
+      hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
+    };
+
     prefs = {
       inherit (common.home) shell git;
       cli.enable = true;
--- configs/server/users.nix
+++ configs/server/users.nix
@@ -1,7 +1,10 @@
 { common, ... }:
 {
   trivaris = {
-    infos.hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
+    infos = {
+      name = "trivaris";
+      hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
+    };
     prefs = {
       inherit (common.home) git shell terminalEmulator;
       cli.enable = true;
