0dd441003e31360b754426fbb821d678894cf8c1
trivaris <git@trivaris.org>
2026-01-08 13:01:25 +0100

    fix: monitors

--- configs/laptop/users.nix
+++ configs/laptop/users.nix
@@ -5,7 +5,7 @@
       name = "trivaris";
       hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
     };
-    
+
     prefs = {
       inherit (common.home)
         appLauncher
