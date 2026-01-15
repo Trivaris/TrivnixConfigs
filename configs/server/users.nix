25d761b007237cb748ea613ec84261aecee322cc
trivaris <git@trivaris.org>
2026-01-15 12:10:23 +0100

    update: update

--- configs/server/users.nix
+++ configs/server/users.nix
@@ -4,6 +4,7 @@
     infos = {
       name = "trivaris";
       hashedPassword = "$y$j9T$ZYnirABSLjZi.mfiKE.Si0$Fe67Vw2MHKtZybsHVGf0rNqcnN67LgLdsCbM8q6ln/B";
+      uid = 1000;
     };
     prefs = {
       inherit (common.home) git shell terminalEmulator;
