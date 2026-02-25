25d24013ea8cc631a66961be39847fb455a57344
trivaris <git@trivaris.org>
2026-02-25 12:47:12 +0100

    feat: somehting idek

--- configs/laptop/hardware.nix
+++ configs/laptop/hardware.nix
@@ -5,54 +5,75 @@
   ...
 }:
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  powerManagement.powertop.enable = true;
-  services.thermald.enable = true;
-  services.power-profiles-daemon.enable = false;
-  services.tlp = {
+  powerManagement = {
     enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    powertop.enable = true;
+  };
 
-      WIFI_PWR_ON_BAT = "on";
-      CPU_BOOST_ON_BAT = 0;
+  services = {
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+          energy_performance_preference = "power";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
 
-      START_CHARGE_THRESH_BAT0 = 40; 
-      STOP_CHARGE_THRESH_BAT0 = 90;
+    system76-scheduler.settings.cfsProfiles.enable = true;
+  };
+
+  boot = {
+    kernelModules = [ "kvm-amd" ];
+    
+    kernelParams = [
+      "amd_pstate=active"
+      "nvme.noacpi=1"
+      "ahci.mobile_lpm_policy=3"
+      "rtc_cmos.use_acpi_alarm=1"
+    ];
+
+    extraModprobeConfig = ''
+      options rtw89_pci disable_aspm=n
+      options rtw89_core disable_ps_mode=n
+    '';
+
+    blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+
+    initrd.availableKernelModules = [
+      "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"
+    ];
+    
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
   };
-  
-  boot.extraModprobeConfig = ''
-    options rtw89_pci disable_aspm=n
-    options rtw89_core disable_ps_mode=n
-  '';
-  boot.initrd.kernelModules = [ ];
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci_renesas"
-    "xhci_pci"
-    "usb_storage"
-    "sd_mod"
-    "rtsx_pci_sdmmc"
-  ];
-  
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.kernelParams = [ "amd_pstate=active" "rtc_cmos.use_acpi_alarm=1" ];
-  boot.extraModulePackages = [ ];
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.graphics.enable32Bit = true;
-  hardware.graphics.enable = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
 
-  networking.useDHCP = lib.mkDefault true;
-  networking.hostName = config.hostInfos.name;
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = config.hostInfos.name;
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault config.hostInfos.architecture;
   system.stateVersion = config.hostInfos.stateVersion;
